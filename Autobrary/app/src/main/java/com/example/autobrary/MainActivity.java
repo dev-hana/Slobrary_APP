dcfa4c6128524faa2402b0a2cb5011e48ee45521
sangik-lee <dev_sang@naver.com>
2020-04-03 15:05:56 +0900

    로그인버튼 데이터값 검증생성 및 로그인,회원가입 게터세터 파일 생성

--- Autobrary/app/src/main/java/com/example/autobrary/MainActivity.java
+++ Autobrary/app/src/main/java/com/example/autobrary/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.autobrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtId, edtPw;
@@ -26,10 +29,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         String result = task.getResult();
-
         System.out.println(result);
 
+        Button loginButton = (Button)findViewById(R.id.btnLog);
+        loginButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText idField = (EditText)findViewById(R.id.edtId);
+                EditText pwField = (EditText)findViewById(R.id.edtPw);
+                if(idField.getText().toString().getBytes().length <= 0 || pwField.getText().toString().getBytes().length <= 0){
+                    Toast.makeText(MainActivity.this, "아이디 와 비밀번호 모두 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
