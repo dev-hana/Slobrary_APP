6d5f9e95019a843bcdc5be1e1265398efd43edde
sangik-lee <dev_sang@naver.com>
2020-04-05 17:28:32 +0900

    회원가입 xml화면 추가 및 엑티비티 클래스 추가

---
+++ Autobrary/app/src/main/java/com/example/autobrary/auth/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.autobrary.auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.autobrary.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
