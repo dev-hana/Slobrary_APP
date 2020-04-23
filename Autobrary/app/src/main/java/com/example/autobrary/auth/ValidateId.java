abb2b7c02b4116de8de2732995b4eb1aa83e020a
sangik-lee <dev_sang@naver.com>
2020-04-23 16:20:51 +0900

    아이디 중복체크 모듈 생성

---
+++ Autobrary/app/src/main/java/com/example/autobrary/auth/ValidateId.java
@@ -0,0 +1,63 @@
+package com.example.autobrary.auth;
+
+import com.example.autobrary.database.URLConnector;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class ValidateId {
+    private String id;
+
+    /**
+     * 아이디의 중복여부를 리턴 합니다.
+     *
+     * @param   id    중복 검증할 아이디
+     * @return   boolean    아이디가 존재하면 true, 존재하지 않으면 false를 리턴
+     */
+    public boolean IdCheck(String id) throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
+        this.id = id;
+        return execute();
+    }
+    private boolean execute() {
+
+        String REQUEST_PAGE = "Join_check.php";
+        HashMap<String, String> param = new HashMap<>();
+
+        // 파라미터 입력
+        param.put("mem_id", id);
+
+        HttpEntity rawData = null;
+        BufferedInputStream bis = null;
+        String result = "false";
+        boolean validateResult = false;
+        try {
+            URLConnector task = new URLConnector(REQUEST_PAGE, param);
+            task.start();
+            task.join();
+            HttpResponse resp = task.getResult();
+            rawData = resp.getEntity();
+            result = EntityUtils.toString(rawData);
+            if (new JSONObject(result).getString("success").equals("true")) {
+                validateResult = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                if(bis != null) bis.close();
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+        return validateResult;
+    }
+}
