1bf0d8f161faafe2bca186e2f187f33eac1573fb
sangik-lee <dev_sang@naver.com>
2020-05-05 17:23:13 +0900

    공지사항 연동 모듈 완성

--- Autobrary/app/src/main/java/com/example/autobrary/notice/Notice.java
+++ Autobrary/app/src/main/java/com/example/autobrary/notice/Notice.java
@@ -1,5 +1,8 @@
 package com.example.autobrary.notice;
 
+import android.os.Debug;
+import android.util.Log;
+
 import com.example.autobrary.auth.LoginInfo;
 import com.example.autobrary.database.URLConnector;
 import com.example.autobrary.encryption.PBKDF2_Encryption;
@@ -10,21 +13,23 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
 
 import cz.msebera.android.httpclient.HttpEntity;
 import cz.msebera.android.httpclient.HttpResponse;
 import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class Notice {
-    public boolean execute() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-
-        String REQUEST_PAGE = "Login.php";
+    public Vector<NoticeInfo> execute() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        Vector<NoticeInfo> notice = new Vector<>();
+        String REQUEST_PAGE = "Notice_all.php";
 
         HttpEntity rawData = null;
         BufferedInputStream bis = null;
         String result = "false";
-        boolean validateResult = false;
         try {
             URLConnector task = new URLConnector(REQUEST_PAGE, new HashMap());
             task.start();
@@ -32,7 +37,24 @@ public class Notice {
             HttpResponse resp = task.getResult();
             rawData = resp.getEntity();
             result = EntityUtils.toString(rawData);
-
+            JSONObject jsonResult = new JSONObject(result);
+            if(jsonResult.getString("success").equals("true")) {
+                ArrayList<String> jsonKeyList = new ArrayList<>();
+                Iterator i = jsonResult.keys();
+                while (i.hasNext()) {
+                    String b = i.next().toString();
+                    jsonKeyList.add(b);
+                }
+                for(int j = 1; j < jsonKeyList.size(); j++){
+                    String title = new JSONObject(jsonResult.getString(Integer.toString(j))).getString("name");
+                    String date = new JSONObject(jsonResult.getString(Integer.toString(j))).getString("date");
+                    String owner = new JSONObject(jsonResult.getString(Integer.toString(j))).getString("admin_id");
+                    NoticeInfo fetchNotice = new NoticeInfo(title, date, owner);
+                    notice.add(fetchNotice);
+                }
+            }else{
+                Log.e("Notice Error", "Notice fetch failed");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
@@ -42,6 +64,6 @@ public class Notice {
                 e2.printStackTrace();
             }
         }
-        return validateResult;
+        return notice;
     }
 }
