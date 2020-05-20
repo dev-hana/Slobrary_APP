856a4b17f62df9249781ccfa5497a985523f6e6c
sangik-lee <dev_sang@naver.com>
2020-05-20 21:03:43 +0900

    wish(신청도서) info 관련 생성

---
+++ Autobrary/app/src/main/java/com/example/autobrary/wish/WishInfo.java
@@ -0,0 +1,41 @@
+package com.example.autobrary.wish;
+
+public class WishInfo {
+    private String user;
+    private String bookTitle;
+    private String bookOwner;
+    private String bookPublish;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookOwner() {
+        return bookOwner;
+    }
+
+    public void setBookOwner(String bookOwner) {
+        this.bookOwner = bookOwner;
+    }
+
+    public String getBookPublish() {
+        return bookPublish;
+    }
+
+    public void setBookPublish(String bookPublish) {
+        this.bookPublish = bookPublish;
+    }
+
+}
