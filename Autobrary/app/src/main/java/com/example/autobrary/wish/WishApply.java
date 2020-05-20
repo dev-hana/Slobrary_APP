e5db2efe812e2c42e222ba1a6c1d2f9f87ee244b
sangik-lee <dev_sang@naver.com>
2020-05-20 21:07:59 +0900

    신청도서 웹php 연동 모듈 완성

---
+++ Autobrary/app/src/main/java/com/example/autobrary/wish/WishApply.java
@@ -0,0 +1,57 @@
+package com.example.autobrary.wish;
+
+import com.example.autobrary.auth.LoginInfo;
+import com.example.autobrary.database.URLConnector;
+import com.example.autobrary.encryption.PBKDF2_Encryption;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.HttpEntity;
+
+public class WishApply {
+    WishInfo info;
+    public WishApply(WishInfo info) {
+        this.info = info;
+    }
+
+    public boolean execute() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+
+        String REQUEST_PAGE = ""; //TODO : PHP 경로 입력
+        HashMap<String, String> param = new HashMap<>();
+
+        // 파라미터 입력
+        param.put("bookTitle", info.getBookTitle());
+        param.put("bookOwner", info.getBookOwner());
+        param.put("bookPublish", info.getBookPublish());
+        param.put("applyUser", info.getUser());
+
+        HttpEntity rawData = null;
+        BufferedInputStream bis = null;
+        String result = "false";
+        boolean validateResult = false;
+        try {
+            URLConnector task = new URLConnector(REQUEST_PAGE, param);
+            task.start();
+            task.join();
+            result = task.getData();
+            if (new JSONObject(result).getString("success").equals("true")) {
+                validateResult = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) bis.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return validateResult;
+    }
+}
