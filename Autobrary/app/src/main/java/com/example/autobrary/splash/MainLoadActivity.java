db4197a01855172e0cf517c1589621c11e864977
devstar1224 <dev_sang@naver.com>
2020-06-12 11:34:55 +0900

    첫 로딩 스플레쉬 화면 추가

---
+++ Autobrary/app/src/main/java/com/example/autobrary/splash/MainLoadActivity.java
@@ -0,0 +1,30 @@
+package com.example.autobrary.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.autobrary.R;
+import com.example.autobrary.main.Rpage;
+
+public class MainLoadActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), Rpage.class);  // Intent 선언
+                startActivity(intent);   // Intent 시작
+                finish();
+            }
+        }, 2000);  // 로딩화면 시간
+    }
+
+
+}
